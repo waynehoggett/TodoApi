e76b00260705fe8c51ff1b3afeeff41e3626c4a8
Wayne Hoggett <whoggett@auscript.com.au>
2023-10-06 14:17:52 +1000

    Updating API Name

--- Program.cs
+++ Program.cs
@@ -7,6 +7,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.ConfigureSwaggerGen(setup =>
+{
+    setup.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Weather Forecasts",
+        Version = "v1"
+    });
+});
+
 var app = builder.Build();
 
 app.UseSwagger();
